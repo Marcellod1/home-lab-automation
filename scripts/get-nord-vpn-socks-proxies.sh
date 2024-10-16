4bcc95e29ee6dd4617991d0b32bf3188f3a1f880
Marcello Di Benedetto <marcellodb17@gmail.com>
2024-10-16 17:15:35 -0600

    Script to get NordVPN SOCKS5 proxies

--- scripts/get-nord-vpn-socks-proxies.sh
+++ scripts/get-nord-vpn-socks-proxies.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s "https://api.nordvpn.com/v1/servers?limit=10000" | jq -r ".[] | select(.technologies[].id | contains(21)) | select(.locations[].country.code | contains(\"CA\")) | .hostname"
+curl -s "https://api.nordvpn.com/v1/servers?limit=10000" | jq -r ".[] | select(.technologies[].id | contains(7)) | .hostname"
